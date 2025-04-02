06bf9791ce0109464ad8bbdbda20f935047c521b
kalr <karlmiguel01001011@gmail.com>
2025-04-02 22:50:21 +0800

    first commit

--- prison_management.sql
+++ prison_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2025 at 04:23 PM
+-- Generation Time: Apr 02, 2025 at 04:48 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,56 +24,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `prisoners`
+-- Table structure for table `visitors`
 --
 
-CREATE TABLE `prisoners` (
+CREATE TABLE `visitors` (
   `id` int(11) NOT NULL,
   `full_name` varchar(255) NOT NULL,
-  `id_number` varchar(50) NOT NULL,
-  `age` int(11) NOT NULL,
   `contact_number` varchar(20) DEFAULT NULL,
-  `prisoner_case` text NOT NULL,
-  `detained_date` date NOT NULL,
-  `cell_number` varchar(50) DEFAULT NULL,
-  `sentence_start` date DEFAULT NULL,
-  `sentence_end` date DEFAULT NULL,
-  `gender` enum('Male','Female','Other') NOT NULL
+  `visit_purpose` varchar(255) NOT NULL,
+  `address` text DEFAULT NULL,
+  `prisoner_id` int(11) NOT NULL,
+  `relationship_to_prisoner` varchar(50) DEFAULT NULL,
+  `visit_date` date NOT NULL,
+  `visit_time` time NOT NULL,
+  `visit_time_in` time DEFAULT NULL,
+  `visit_time_out` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `prisoners`
+-- Dumping data for table `visitors`
 --
 
-INSERT INTO `prisoners` (`id`, `full_name`, `id_number`, `age`, `contact_number`, `prisoner_case`, `detained_date`, `cell_number`, `sentence_start`, `sentence_end`, `gender`) VALUES
-(1, 'Female', '123456789', 41, '09876543234', 'Robbery BOBa', '2025-03-28', 'lalalawefsdf', '2025-03-21', '2035-01-24', 'Other'),
-(3, 'Ady', '32433435465456575676', 23, '09873234556', 'Nanagasad', '2025-03-09', 'KJASsadDasdJK', '2025-02-26', '1954-03-25', 'Male'),
-(4, 'Althea Soriano', '3435435', 43, '09876543212', 'Namababae', '2025-12-09', 'asdewrert', '2025-03-04', '2046-04-04', 'Other'),
-(5, 'JEJEJE', '99843892', 55, '32453465675', 'Naglakad', '2025-03-20', 'asdasd', '2025-03-26', '2025-07-31', 'Female'),
-(6, 'weadsda', '325345', 32, '32142354', '324rdfgfhe56trgf', '2025-03-20', '14/02/2025', '2025-01-29', '2025-02-27', 'Male'),
-(7, 'sdfsdf', '4354645', 122, '675654675', '645rtgfbgfhg', '2025-03-30', 'df546434', '2025-03-30', '2025-03-30', 'Female'),
-(8, 'Karl Perez', '43547567654356754345', 122, '45456578796', 'Nambully', '2025-03-30', '34B', '2025-03-06', '2025-05-22', 'Female');
+INSERT INTO `visitors` (`id`, `full_name`, `contact_number`, `visit_purpose`, `address`, `prisoner_id`, `relationship_to_prisoner`, `visit_date`, `visit_time`, `visit_time_in`, `visit_time_out`) VALUES
+(1, 'Jeffrey Bonina Nakatakas', '32435345464', 'Namimiss', 'Phase 8', 7, 'Church', '2025-04-09', '00:00:00', '22:00:51', '22:00:51'),
+(3, 'Cielo Corda', '09213456578', 'Medyo namiss', 'Shelterville', 9, 'Church', '2025-04-23', '00:00:00', '21:01:18', '22:04:18');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `prisoners`
+-- Indexes for table `visitors`
 --
-ALTER TABLE `prisoners`
+ALTER TABLE `visitors`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_number` (`id_number`);
+  ADD KEY `prisoner_id` (`prisoner_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `prisoners`
+-- AUTO_INCREMENT for table `visitors`
 --
-ALTER TABLE `prisoners`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `visitors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `visitors`
+--
+ALTER TABLE `visitors`
+  ADD CONSTRAINT `visitors_ibfk_1` FOREIGN KEY (`prisoner_id`) REFERENCES `prisoners` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
